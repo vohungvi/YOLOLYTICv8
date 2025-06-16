6c430c541154df21376cba17a20ee9459208ddc7
vohungvi <vohungvi27@gmail.com>
2025-06-16 20:46:01 +0700

    + add example

---
+++ Example/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TGMT;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "e-class.jpg";
+
+            //khởi tạo detector bằng cách load model và class name
+            YOLOv8Detector detector = new YOLOv8Detector(@"..\pretrain\yolov8n_cuda.onnx", @"..\pretrain\coco.names", 640, 640);
+
+            //Detect ảnh hoặc đường dẫn ảnh
+            bool drawRect = true;
+            bool drawText = true;
+            Predicted predicted = detector.Detect("e-class.jpg", drawRect, drawText);
+
+            //hiển thị ảnh đã vẽ bouding box lên picture box
+            pictureBox2.Image = predicted.bitmap;
+
+            if(predicted.objects == null || predicted.objects.Length == 0)
+            {
+                MessageBox.Show("Không phát hiện được đối tượng nào trong ảnh");
+                this.Text = "Detected: 0";
+                return;
+            }
+
+            //in ra danh sách các object
+            for (int i = 0; i<predicted.objects.Length; i++)
+            {
+                YOLOobject obj = predicted.objects[i];
+                Console.WriteLine(String.Format("{0}: {1}", obj.className, obj.confident.ToString()));
+            }
+
+            this.Text = "Detected: " + predicted.objects.Length.ToString();
+
+        }
+    }
+}
